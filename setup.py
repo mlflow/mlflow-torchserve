19360c99b0051a610148fb9ad0f738aa6a666510
harupy <17039389+harupy@users.noreply.github.com>
2020-11-03 14:05:01 +0900

    Add long_description in setup.py for PyPI release
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="mlflow-torchserve",
     version="0.0.1",
     description="Torch Serve Mlflow Deployment",
+    long_description=open("README.md").read(),
     packages=find_packages(),
     # Require MLflow as a dependency of the plugin, so that plugin users can simply install
     # the plugin & then immediately use it with MLflow
