b9bdb2391304935089bc37aea3ae4370350f9546
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-05 22:59:46 +0530

    Running python tests first to avoid conda env issues
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- tests/examples/test_torchserve_examples.py
+++ tests/examples/test_torchserve_examples.py
@@ -19,11 +19,29 @@ def find_conda_yaml(directory):
 
 @pytest.mark.large
 @pytest.mark.parametrize(
+    "directory, command",
+    [
+        ("MNIST/example1", ["python", "mnist_model.py", "--epochs", "1"]),
+        ("MNIST/example2", ["python", "mnist_model.py", "--epochs", "1"]),
+        # ("MNIST/example3", ["python", "mnist_model.py", "--epochs", "1"]),
+        (
+            "BertNewsClassification",
+            ["python", "news_classifier.py", "--epochs", "1", "--num-samples", "200"],
+        ),
+    ],
+)
+def test_command_example(directory, command):
+    cwd_dir = os.path.join(EXAMPLES_DIR, directory)
+    process.exec_cmd(command, cwd=cwd_dir)
+
+
+@pytest.mark.large
+@pytest.mark.parametrize(
     "directory, params",
     [
         ("MNIST/example1", ["-P", "epochs=1"]),
         ("MNIST/example2", ["-P", "epochs=1"]),
-        ("MNIST/example3", ["-P", "epochs=1"]),
+        # ("MNIST/example3", ["-P", "epochs=1"]),
         ("BertNewsClassification", ["-P", "epochs=1", "-P", "num_samples=100"]),
     ],
 )
@@ -34,21 +52,3 @@ def test_mlflow_run_example(directory, params):
     assert res.exit_code == 0, "Got non-zero exit code {0}. Output is: {1}".format(
         res.exit_code, res.output
     )
-
-
-@pytest.mark.large
-@pytest.mark.parametrize(
-    "directory, command",
-    [
-        ("MNIST/example1", ["python", "mnist_model.py", "--epochs", "1"]),
-        ("MNIST/example2", ["python", "mnist_model.py", "--epochs", "1"]),
-        ("MNIST/example3", ["python", "mnist_model.py", "--epochs", "1"]),
-        (
-            "BertNewsClassification",
-            ["python", "news_classifier.py", "--epochs", "1", "--num-samples", "200"],
-        ),
-    ],
-)
-def test_command_example(directory, command):
-    cwd_dir = os.path.join(EXAMPLES_DIR, directory)
-    process.exec_cmd(command, cwd=cwd_dir)
