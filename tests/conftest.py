e6b8160e61184779e0d3065df7c634ef9a80e0c9
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-06 18:56:25 +0530

    Reusing fixtures across files
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

---
+++ tests/conftest.py
@@ -0,0 +1,1 @@
+from tests.fixtures.plugin_fixtures import start_torchserve, stop_torchserve, health_checkup
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,16 +1,10 @@
-import atexit
 import json
 import os
-import shutil
-import subprocess
-import time
-import torch
 
 import pytest
-
+import torch
 from mlflow import deployments
 from mlflow.exceptions import MlflowException
-from tests.resources import linear_model
 
 f_target = "torchserve"
 f_deployment_id = "test"
@@ -24,51 +18,6 @@ model_file_path = os.path.join("tests/resources", "linear_model.py")
 handler_file_path = os.path.join("tests/resources", "linear_handler.py")
 sample_input_file = os.path.join("tests/resources", "sample.json")
 sample_output_file = os.path.join("tests/resources", "output.json")
-
-
-@pytest.fixture(scope="session")
-def start_torchserve():
-    linear_model.main()
-    if not os.path.isdir("model_store"):
-        os.makedirs("model_store")
-    cmd = "torchserve --start --model-store {}".format("./model_store")
-    _ = subprocess.Popen(cmd, shell=True).wait()
-
-    count = 0
-    for _ in range(5):
-        value = health_checkup()
-        if value is not None and value != "" and json.loads(value)["status"] == "Healthy":
-            time.sleep(1)
-            break
-        else:
-            count += 1
-            time.sleep(5)
-    if count >= 5:
-        raise Exception("Unable to connect to torchserve")
-    return True
-
-
-def health_checkup():
-    curl_cmd = "curl http://localhost:8080/ping"
-    (value, _) = subprocess.Popen([curl_cmd], stdout=subprocess.PIPE, shell=True).communicate()
-    return value.decode("utf-8")
-
-
-def stop_torchserve():
-    cmd = "torchserve --stop"
-    _ = subprocess.Popen(cmd, shell=True).wait()
-
-    if os.path.isdir("model_store"):
-        shutil.rmtree("model_store")
-
-    if os.path.exists("tests/resources/linear_state_dict.pt"):
-        os.remove("tests/resources/linear_state_dict.pt")
-
-    if os.path.exists("tests/resources/linear_model.pt"):
-        os.remove("tests/resources/linear_model.pt")
-
-
-atexit.register(stop_torchserve)
 
 
 @pytest.mark.usefixtures("start_torchserve")
