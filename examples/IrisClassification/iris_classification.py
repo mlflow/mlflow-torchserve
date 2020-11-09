e4ebf56eb2dd3cc6c73e0eda852fb38492e1f4a3
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-09 16:31:40 +0530

    IrisClassification - Gpu Fix and using base handler
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- examples/IrisClassification/iris_classification.py
+++ examples/IrisClassification/iris_classification.py
@@ -2,21 +2,24 @@
 # pylint: disable=W0613
 # pylint: disable=W0223
 import argparse
+from argparse import ArgumentParser
+
 import pytorch_lightning as pl
-from pytorch_lightning.metrics.functional import accuracy
-from sklearn.datasets import load_iris
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, random_split, TensorDataset
+from pytorch_lightning.metrics import Accuracy
 
 
 class IrisClassification(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(IrisClassification, self).__init__()
-        self.train_set = None
-        self.val_set = None
-        self.test_set = None
+
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
+        self.args = kwargs
+
         self.fc1 = nn.Linear(4, 10)
         self.fc2 = nn.Linear(10, 10)
         self.fc3 = nn.Linear(10, 3)
@@ -26,68 +29,64 @@ class IrisClassification(pl.LightningModule):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.softmax(x, dim=1)
         return x
 
-    def prepare_data(self):
-        iris = load_iris()
-        df = iris.data
-        target = iris["target"]
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        """
+        Add model specific arguments like learning rate
 
-        data = torch.Tensor(df).float()
-        labels = torch.Tensor(target).long()
+        :param parent_parser: Application specific parser
 
-        data_set = TensorDataset(data, labels)
-        self.train_set, self.val_set = random_split(data_set, [130, 20])
-        self.train_set, self.test_set = random_split(self.train_set, [110, 20])
-
-    def train_dataloader(self):
-        train_loader = DataLoader(dataset=self.train_set, batch_size=8, num_workers=3)
-        return train_loader
-
-    def val_dataloader(self):
-        validation_loader = DataLoader(dataset=self.val_set, batch_size=8, num_workers=3)
-        return validation_loader
-
-    def test_dataloader(self):
-        test_loader = DataLoader(dataset=self.test_set, batch_size=8, num_workers=3)
-        return test_loader
+        :return: Returns the augmented arugument parser
+        """
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument(
+            "--lr",
+            type=float,
+            default=0.01,
+            metavar="LR",
+            help="learning rate (default: 0.001)",
+        )
+        return parser
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.02)
+        return torch.optim.Adam(self.parameters(), self.args["lr"])
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         logits = self.forward(x)
         loss = self.cross_entropy_loss(logits, y)
-        self.log("train_loss", loss)
+        self.train_acc(logits, y)
+        self.log(
+            "train_acc",
+            self.train_acc.compute().cpu(),
+            on_step=False,
+            on_epoch=True,
+            sync_dist=True,
+        )
+        self.log("train_loss", loss.cpu(), on_step=False, on_epoch=True, sync_dist=True)
         return {"loss": loss}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self.forward(x)
         loss = F.cross_entropy(logits, y)
-        return {"val_loss": loss}
-
-    def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
-        self.log("val_loss", avg_loss)
+        self.val_acc(logits, y)
+        self.log(
+            "val_acc", self.val_acc.compute().cpu(), on_step=False, on_epoch=True, sync_dist=True
+        )
+        self.log("val_loss", loss.cpu(), on_step=False, on_epoch=True, sync_dist=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         logits = self.forward(x)
         loss = F.cross_entropy(logits, y)
         _, y_hat = torch.max(logits, dim=1)
-        test_acc = accuracy(y_hat.cpu(), y.cpu())
-        self.log("test_loss", loss)
-        self.log("test_acc", test_acc)
-        return {"test_loss": loss, "test_acc": test_acc}
-
-    def test_epoch_end(self, outputs):
-        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
-        avg_test_acc = torch.stack([x["test_acc"] for x in outputs]).mean()
-        self.log("avg_test_loss", avg_loss)
-        self.log("avg_test_acc", avg_test_acc)
+        self.test_acc(y_hat, y)
+        self.log(
+            "test_acc", self.test_acc.compute().cpu(), on_step=False, on_epoch=True, sync_dist=True
+        )
 
 
 if __name__ == "__main__":
@@ -97,18 +96,36 @@ if __name__ == "__main__":
         "--max-epochs", type=int, default=100, help="number of epochs to run (default: 100)"
     )
     parser.add_argument(
+        "--gpus", type=int, default=0, help="Number of gpus - by default runs on CPU"
+    )
+    parser.add_argument(
         "--save-model",
         type=bool,
         default=True,
         help="For Saving the current Model",
     )
+    parser.add_argument(
+        "--accelerator",
+        type=lambda x: None if x == "None" else x,
+        default=None,
+        help="Accelerator - (default: None)",
+    )
+
+    from iris_data_module import IrisDataModule
+
+    parser = IrisClassification.add_model_specific_args(parent_parser=parser)
+    parser = IrisDataModule.add_model_specific_args(parent_parser=parser)
 
     args = parser.parse_args()
+    dict_args = vars(args)
 
-    model = IrisClassification()
+    dm = IrisDataModule(**dict_args)
+    dm.prepare_data()
+    dm.setup(stage="fit")
+
+    model = IrisClassification(**dict_args)
     trainer = pl.Trainer.from_argparse_args(args)
-    trainer.fit(model)
+    trainer.fit(model, dm)
     trainer.test()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "iris.pt")
+    torch.save(model.state_dict(), "iris.pt")
--- examples/IrisClassification/iris_handler.py
+++ examples/IrisClassification/iris_handler.py
@@ -1,54 +1,21 @@
 import ast
 import logging
-import os
 
 import numpy as np
 import torch
-
-from iris_classification import IrisClassification
+from ts.torch_handler.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
 
 
-class IRISClassifierHandler(object):
+class IRISClassifierHandler(BaseHandler):
     """
     IRISClassifier handler class. This handler takes an input tensor and
     output the type of iris based on the input
     """
 
     def __init__(self):
-        self.model = None
-        self.mapping = None
-        self.device = None
-        self.initialized = False
-
-    def initialize(self, ctx):
-        """
-        First try to load torchscript else load eager mode state_dict based model
-
-        :param ctx: System properties
-        """
-        properties = ctx.system_properties
-        self.device = torch.device(
-            "cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu"
-        )
-        model_dir = properties.get("model_dir")
-
-        # Read model serialize/pt file
-        model_pt_path = os.path.join(model_dir, "iris.pt")
-        # Read model definition file
-        model_def_path = os.path.join(model_dir, "iris_classification.py")
-        if not os.path.isfile(model_def_path):
-            raise RuntimeError("Missing the model definition file")
-
-        state_dict = torch.load(model_pt_path, map_location=self.device)
-        self.model = IrisClassification()
-        self.model.load_state_dict(state_dict)
-        self.model.to(self.device)
-        self.model.eval()
-
-        logger.debug("Model file %s loaded successfully", model_pt_path)
-        self.initialized = True
+        super(IRISClassifierHandler, self).__init__()
 
     def preprocess(self, data):
         """
@@ -59,7 +26,6 @@ class IRISClassifierHandler(object):
         :return: output - Preprocessed input
         """
 
-        print(data)
         input_data_str = data[0].get("data")
         if input_data_str is None:
             input_data_str = data[0].get("body")
@@ -93,29 +59,10 @@ class IRISClassifierHandler(object):
 
         :return: output - Output after post processing
         """
+
+        if self.mapping:
+            return [self.mapping[str(inference_output[0])]]
         return inference_output
 
 
 _service = IRISClassifierHandler()
-
-
-def handle(data, context):
-    """
-    Default function that is called when predict is invoked
-
-    :param data: Input to be passed through the layers for prediction
-    :param context: dict containing system properties
-
-    :return: output - Output after postprocess
-    """
-    if not _service.initialized:
-        _service.initialize(context)
-
-    if data is None:
-        return None
-
-    data = _service.preprocess(data)
-    data = _service.inference(data)
-    data = _service.postprocess(data)
-
-    return data
