1fc7cab230ced419528b51c6f9439c5fbf1f4c9c
Shrinath Suresh <shrinath@ideas2it.com>
2020-11-11 20:36:08 +0530

    Adding argument parser for create and delete deployments and updating readme
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

---
+++ examples/MNIST/create_deployment.py
@@ -0,0 +1,69 @@
+from argparse import ArgumentParser
+
+from mlflow.deployments import get_deploy_client
+
+
+def create_deployment(parser_args):
+    plugin = get_deploy_client(parser_args["target"])
+    config = {
+        "MODEL_FILE": parser_args["model_file"],
+        "HANDLER": parser_args["handler"],
+        "EXTRA_FILES": parser_args["extra_files"],
+    }
+    result = plugin.create_deployment(
+        name=parser_args["deployment_name"],
+        model_uri=parser_args["serialized_file"],
+        config=config,
+    )
+
+    print("Deployment {result} created successfully".format(result=result["name"]))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="MNIST hand written digits classification example")
+
+    parser.add_argument(
+        "--target",
+        type=str,
+        default="torchserve",
+        help="MLflow target (default: torchserve)",
+    )
+
+    parser.add_argument(
+        "--deployment_name",
+        type=str,
+        default="mnist_classification",
+        help="Deployment name (default: mnist_classification)",
+    )
+
+    parser.add_argument(
+        "--model_file",
+        type=str,
+        default="mnist_model.py",
+        help="Model file path (default: mnist_model.py)",
+    )
+
+    parser.add_argument(
+        "--handler",
+        type=str,
+        default="mnist_handler.py",
+        help="Handler file path (default: mnist_handler.py)",
+    )
+
+    parser.add_argument(
+        "--extra_files",
+        type=str,
+        default="index_to_name.json",
+        help="List of extra files",
+    )
+
+    parser.add_argument(
+        "--serialized_file",
+        type=str,
+        default="model",
+        help="Pytorch model path (default: model)",
+    )
+
+    args = parser.parse_args()
+
+    create_deployment(vars(args))
---
+++ examples/MNIST/predict.py
@@ -0,0 +1,47 @@
+import os
+from argparse import ArgumentParser
+
+import matplotlib.pyplot as plt
+from mlflow.deployments import get_deploy_client
+from torchvision import transforms
+
+
+def predict(parser_args):
+    plugin = get_deploy_client(parser_args["target"])
+    img = plt.imread(os.path.join(parser_args["input_file_path"]))
+    mnist_transforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
+
+    image_tensor = mnist_transforms(img)
+    prediction = plugin.predict(parser_args["deployment_name"], image_tensor)
+    print("Prediction Result {}".format(prediction))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="MNIST hand written digits classification example")
+
+    parser.add_argument(
+        "--target",
+        type=str,
+        default="torchserve",
+        help="MLflow target (default: torchserve)",
+    )
+
+    parser.add_argument(
+        "--deployment_name",
+        type=str,
+        default="mnist_classification",
+        help="Deployment name (default: mnist_classification)",
+    )
+
+    parser.add_argument(
+        "--input_file_path",
+        type=str,
+        default="test_data/one.png",
+        help="Path to input image for prediction (default: test_data/one.png)",
+    )
+
+    args = parser.parse_args()
+
+    predict(vars(args))
