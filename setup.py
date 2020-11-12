edce6e4a9c9a9a42387660424be36940c6fffcb2
Geeta Chauhan <4461127+chauhang@users.noreply.github.com>
2020-11-12 01:30:11 -0600

    Update release version
    Change the version for the v0.1.0 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlflow-torchserve",
-    version="0.0.1",
+    version="0.1.0",
     description="Torch Serve Mlflow Deployment",
     long_description=open("README.md").read(),
     packages=find_packages(),
