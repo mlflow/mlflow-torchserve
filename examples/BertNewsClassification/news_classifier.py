Fix bug in accuracy calculation
The result of `torch.sum(preds == targets)` could be as much as `self.BATCH_SIZE`, so `len(data_loader)` isn't enough of a denominator.

There could be batches from the `data_loader` with less than `BATCH_SIZE`, so a running counter of would be more accurate, but I think this is good enough.

Signed-off-by: John Savage <jsavage@overstock.com>

examples/BertNewsClassification/news_classifier.py
@@ -289,7 +289,7 @@ class NewsClassifier(nn.Module):
                 correct_predictions += torch.sum(preds == targets)
                 losses.append(loss.item())
 
-        return correct_predictions.double() / len(data_loader), np.mean(losses)
+        return correct_predictions.double() / len(data_loader) / self.BATCH_SIZE, np.mean(losses)
 
     def get_predictions(self, model, data_loader):
 