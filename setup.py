516a912ad1daaa61024c1fea93f4a8bc901b8548
Shrinath Suresh <shrinath@ideas2it.com>
2021-07-06 23:59:32 +0530

    Fixing mlflow master url
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Require MLflow as a dependency of the plugin, so that plugin users can simply install
     # the plugin & then immediately use it with MLflow
     install_requires=[
-        "mlflow@git+https://github.com/chauhang/mlflow.git@master",
+        "mlflow@git+https://github.com/mlflow/mlflow.git@master",
         "torchserve",
         "torch-model-archiver",
     ],
