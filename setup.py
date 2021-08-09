35d28e7681612d30f5650ec1e900a665cd31b065
Shrinath Suresh <shrinath@ideas2it.com>
2021-08-09 21:39:38 +0530

    Tagging release version to 0.2.0
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlflow-torchserve",
-    version="0.1.0",
+    version="0.2.0",
     description="Torch Serve Mlflow Deployment",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
